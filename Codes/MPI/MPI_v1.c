82ec585f346f1ade63abd47f284906be5a1bc7b6
QuartoTR <tristan.michel.1@etu.sorbonne-universite.fr>
2021-05-18 21:49:32 +0200

    MPI architecture

--- Codes/MPI/MPI_v1.c
+++ Codes/MPI/MPI_v1.c
@@ -3,7 +3,41 @@
 #include <cover_functions.h>
 
 
-void solve_MPI(const struct instance_t *instance, struct context_t *ctx, MPI_Status status, int nb_proc, int my_rank)
+int nb_MPI_proc, my_MPI_rank;
+
+
+void receive_pb_data(struct instance_t * instance, struct context_t * ctx) 
+{
+	int *to_recv = malloc(2 * sizeof(int));
+
+	MPI_Bcast(to_recv, 3, MPI_INT, 0, MPI_COMM_WORLD);
+
+	printf("Reçu : %d / %d / %d\n", to_recv[0], to_recv[1], to_recv[2]);
+	int *recv_opt = malloc(to_recv[2] * sizeof(int));
+	int *recv_items = malloc(to_recv[1] * sizeof(int));
+
+	MPI_Bcast(recv_opt, to_recv[2], MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Bcast(recv_items, to_recv[1], MPI_INT, 0, MPI_COMM_WORLD);
+}
+
+void send_pb_data(struct instance_t * instance)
+{
+	instance = load_matrix(in_filename);
+
+	printf("Nombre d'items : %d\n", instance->n_items);
+	printf("Nombre primary : %d\n", instance->n_primary);
+	printf("Nombre d'options : %d\n", instance->n_options);
+	printf("Taille tableau options : %d\n", instance->n_items * instance->n_options);
+
+	int to_send[] = {instance->n_items, instance->n_primary, instance->n_options};
+
+	MPI_Bcast(to_send, 3, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Bcast(instance->options, instance->n_options, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Bcast(instance->ptr, instance->n_items, MPI_INT, 0, MPI_COMM_WORLD);
+}
+
+
+void solve_MPI(const struct instance_t * instance, struct context_t * ctx, MPI_Status status)
 {
 	ctx->nodes++;
 	
@@ -25,7 +59,7 @@ void solve_MPI(const struct instance_t *instance, struct context_t *ctx, MPI_Sta
 		int option = active_options->p[k];
 		ctx->child_num[ctx->level] = k;
 		choose_option(instance, ctx, option, chosen_item);
-		solve_MPI(instance, ctx, status, nb_proc, my_rank);
+		solve(instance, ctx);
 		if (ctx->solutions >= max_solutions)
 			return;
 		unchoose_option(instance, ctx, option, chosen_item);
@@ -33,64 +67,39 @@ void solve_MPI(const struct instance_t *instance, struct context_t *ctx, MPI_Sta
 	uncover(instance, ctx, chosen_item);	/* backtrack */
 }
 
+
 int main(int argc, char **argv)
 {
 	option_setup(argc, argv);
 
-	MPI_Status status;
-	int nb_proc, my_rank;
+	// MPI_Status status;
 
 	MPI_Init(&argc, &argv);
-	MPI_Comm_size(MPI_COMM_WORLD, &nb_proc);
-	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &nb_MPI_proc);
+	MPI_Comm_rank(MPI_COMM_WORLD, &my_MPI_rank);
 
-	MPI_Type_struct(3,B,D,type1,newtype)
-	--> { (2,1,2), (0,16,26), (MPI_FLOAT, {(double,0), (char,8) } , MPI_CHAR), name_of_type }
-	// - Il y aura 3 composantes dans la structure
-	// 	- longueur des données (2 / 1 / 3)
-	// 	- La structure contient 2 FLOAT, 1 map et 3 char
-	// 	- le map est composé d'un double et d'un char 
-	// 	- longueur d'écarts (0,16,26)
-	// 	- le MPI_FLOAT est à 0 / 
-	// 	- le type1 (map) commence à 16 (après 2 float de 8 octet)
-	// 	- le MPI_CHAR commence à 26 octet (16 + 9)
+	// MPI_Type_struct(3, B, D, type1, newtype)
+	// --> { (2,1,2), (0,16,26), (MPI_FLOAT, {(double,0), (char,8) } , MPI_CHAR), name_of_type }
+		// - Il y aura 3 composantes dans la structure
+		// 	- longueur des données (2 / 1 / 3)
+		// 	- La structure contient 2 FLOAT, 1 map et 3 char
+		// 	- le map est composé d'un double et d'un char 
+		// 	- longueur d'écarts (0,16,26)
+		// 	- le MPI_FLOAT est à 0 / 
+		// 	- le type1 (map) commence à 16 (après 2 float de 8 octet)
+		// 	- le MPI_CHAR commence à 26 octet (16 + 9)
 
-	if (my_rank == 0)
-	{
-		struct instance_t * instance = load_matrix(in_filename);
+	struct instance_t * instance = NULL;
+	struct context_t * ctx = NULL;
 
-		int to_send[] = {instance->n_items, instance->n_primary, instance->n_options};
+	if (my_MPI_rank == 0)
+		send_pb_data(instance);
+	else
+		receive_pb_data(instance, ctx);
 
-		MPI_Send(to_send, 3, MPI_INT, 1, 10, MPI_COMM_WORLD);
-		MPI_Send(instance->options, instance->n_options, MPI_INT, 1, 10, MPI_COMM_WORLD);
-		MPI_Send(instance->ptr, instance->n_items, MPI_INT, 1, 9, MPI_COMM_WORLD);
 
-		// printf("Nombre d'items : %d\n",instance->n_items);
-		// printf("Nombre primary : %d\n",instance->n_primary);
-		// printf("Nombre d'options : %d\n",instance->n_options);
-		// printf("Taille tableau options : %d\n",instance->n_items * instance->n_options);
-		// struct context_t * ctx = backtracking_setup(instance);
-		// start = wtime();
-		// solve_MPI(instance, ctx, status, nb_proc, my_rank);
-		// printf("FINI. Trouvé %lld solutions en %.1fs\n\n", ctx->solutions, 
-		// 		wtime() - start);
 
-	}
 
-	if (my_rank == 1)
-	{
-		int *to_recv = malloc(2*sizeof(int));
-
-		MPI_Recv(to_recv, 3, MPI_INT, 0, 10, MPI_COMM_WORLD, &status);
-
-		printf("Reçu : %d / %d / %d\n",to_recv[0], to_recv[1], to_recv[2]);
-		int *recv_opt = malloc(to_recv[2]*sizeof(int));
-		int *recv_items = malloc(to_recv[1]*sizeof(int));
-
-		MPI_Recv(recv_opt, to_recv[2], MPI_INT, 0, 10, MPI_COMM_WORLD, &status);
-		MPI_Recv(recv_items, to_recv[1], MPI_INT, 0, 9, MPI_COMM_WORLD, &status);
-
-	}
 	MPI_Finalize();
 	exit(EXIT_SUCCESS);
 }
